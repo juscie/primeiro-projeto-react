471d9234e28c16622243d7af5bb6d0da71979f31
judelfino <juscie@gmail.com>
2020-04-29 18:15:40 -0500

    3-Navegar entre rotas e estilizar Detalhe PPReact

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -78,7 +79,10 @@ const Dashboard: React.FunctionComponent = () => {
       <span>{inputError && <Error>{inputError}</Error>}</span>
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -88,7 +92,7 @@ const Dashboard: React.FunctionComponent = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+import logoImg from '../../assets/logo.svg';
 
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 //const Repository: React.FC = () => {
 const Repository: React.FunctionComponent = () => {
-  return <h1>Repository</h1>
-}
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="github Exploerer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/27565971?s=460&u=21e5e72bb04e347d742343ecc0e534fe6655da81&v=4"
+            alt="Jusciê"
+          />
+          <div>
+            <strong>juscie/primeiro-projeto-react</strong>
+            <p>Descirção do repositório </p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="aaaa">
+          <div>
+            <strong>aaaaaaa</strong>
+            <p>aaaaaaaa</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Repository;
